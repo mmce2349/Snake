fc058b8fd5314a41ac1ecb783ac4642854f2dd7f
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-05-24 09:43:28 -0600

    made movement possible

--- src/SnakeController/Board.java
+++ src/SnakeController/Board.java
@@ -212,9 +212,36 @@ public class Board extends JPanel implements ActionListener
 	}
 	private class TAdapter extends KeyAdapter
 	{
-		
+		@Override
+		public void keyPressed(KeyEvent e) 
+		{
+			int key = e.getKeyCode();
+			
+			if((key == KeyEvent.VK_LEFT) && (!rightDirection))
+			{
+				leftDirection = true;
+				upDirection = false;
+				downDirection = false;
+			}
+			if((key == KeyEvent.VK_RIGHT) && (!leftDirection))
+			{
+				rightDirection = true;
+				upDirection = false;
+				downDirection = false;
+			}
+			if((key == KeyEvent.VK_UP) && (!downDirection))
+			{
+				upDirection = true;
+				rightDirection = false;
+				leftDirection = false;
+			}
+			if((key == KeyEvent.VK_DOWN) && (!upDirection))
+			{
+				downDirection = true;
+				rightDirection = false;
+				leftDirection = false;
+			}
+			
+		}
 	}
-	
-	
-	
 }
