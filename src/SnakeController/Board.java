9a02b43aec25e5da11e82a2066a1ec926b76cdc9
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-05-24 09:33:04 -0600

    added how to stop timer, and end game

--- src/SnakeController/Board.java
+++ src/SnakeController/Board.java
@@ -157,6 +157,38 @@ public class Board extends JPanel implements ActionListener
 		}
 	}
 	
+	private void checkCollision()
+	{
+		for (int z = dots; z > 0; z--)
+		{
+			if((z > 4) && (x[0] == x[z]) && (y[0] == y[z]))
+			{
+				inGame = false;
+			}
+				
+		}
+		if(y[0] >= boardHeight)
+		{
+			inGame = false;
+		}
+		if(y[0] < 0)
+		{
+			inGame = false;
+		}
+		if (x[0] >= boardWidth)
+		{
+			inGame = false;
+		}
+		if(x[0] < 0)
+		{
+			inGame = false;
+		}
+		if(!inGame)
+		{
+			gameTimer.stop();
+		}
+	}
+	
 	
 	
 	
