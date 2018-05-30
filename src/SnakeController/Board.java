a18db638c317a087255bd66893961d7a4b1c447b
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-05-30 07:49:06 -0600

    tried to fix problems

--- src/SnakeController/Board.java
+++ src/SnakeController/Board.java
@@ -1,7 +1,16 @@
 package SnakeController;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -211,6 +220,7 @@ public class Board extends JPanel implements ActionListener
 		}
 		repaint();
 	}
+	//move directions
 	private class TAdapter extends KeyAdapter
 	{
 		@Override
